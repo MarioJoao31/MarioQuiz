9105ee58c9af556f9068cdd2d36dd8c5ec808c0d
Mário João <50747644+MarioJoao31@users.noreply.github.com>
2020-11-28 16:19:15 +0000

    validação pronta para users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { chec, validationResult, check }= require('express-validator/check');
 
+
+//express-validator para validar post para a bd
 //@rout Post api/users
 //@desc registar user 
 //@access Public
 
-router.post('/', (req,res)=> {
+router.post('/',[
+    check('name','Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Porfavor introduza um email valido')
+        .isEmail(),
+    check('password','Introduza a merda da pass com mais de 6 caracteres')
+        .isLength({ min: 6 })
+    
+], (req,res)=> {
+    const erros= validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
     console.log(req.body);
     res.send('User route')
 });
