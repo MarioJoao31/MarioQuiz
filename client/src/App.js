cb00d3dcd93eb80151db869766aa94703c3cd350
Mário João <50747644+MarioJoao31@users.noreply.github.com>
2020-12-10 00:59:15 +0000

    moving with the hmtl

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import Reac, {Fragment} from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
 
 const App = () => 
   
     <Fragment>
-     <h1>blabla</h1>
+        <Navbar/>
+        <Landing/>
     </Fragment>
 
 
